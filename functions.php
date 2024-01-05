003e1f2f47627a70b8115eb6568c55c076c15652
KingShayne <kingshaynendlovu@gmail.com>
2024-01-05 09:13:31 +0200

    Add bootstrap containers and body_class function and adjust columns properties in shop page

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php 
     function simple_bootstrap_theme_load_scripts(){
 
+        wp_enqueue_style("bootstrap-css", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css", array(),"1.0", 'all' );
         wp_enqueue_style("styles", get_template_directory_uri() . "/assets/css/styles.css", array(),"1.0", 'all' );
         
-        wp_enqueue_script("bootstrap",'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array('jquery'),'1.0', true );
+        wp_enqueue_script("bootstrap-js",'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array('jquery'),'1.0', true );
         wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/scripts.js', array(),'1.0', true );
 
         
@@ -19,7 +20,16 @@
             "secondart_menu_id"=> "Secondary Menu (Footer Menu)"
         ));
 
-        add_theme_support("post-thumbnails");
+        add_theme_support('post-thumbnails');
+        add_theme_support( 'woocommerce', array(
+            'thumbnail_image_width' => 150,
+            'single_image_width' => 200,
+            'product_grid' => array(
+                'default_columns'=> 10,
+                'min_columns'=> 2,
+                'max_columns'=> 3
+            )
+        ));
     }
 
     add_action("after_setup_theme","simple_boostrap_theme_nav_config");
@@ -38,5 +48,17 @@
 
     add_filter("nav_menu_link_attributes","simple_boostrap_theme_add_anchor_links", 1, 3 );
 
-    
+    //remove_action("woocommerce_sidebar", "woocommerce_get_sidebar", 10);
+
+    // function open_container_row_div_classes(){
+    //     echo "<div class='container'><div class='row'>";
+    // }
+
+    // add_action("woocommerce_before_main_content","open_container_row_div_classes", 5);
+
+    // function close_container_row_div_classes(){
+    //     echo "</div></div>";
+    // }
+
+    // add_action("woocommerce_after_main_content","close_container_row_div_classes", 5);
 ?>
--- header.php
+++ header.php
@@ -11,7 +11,7 @@
         
         <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
